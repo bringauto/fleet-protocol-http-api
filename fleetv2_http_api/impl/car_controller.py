86b1918fa22e5c600090c1afdef6dcaeab522186
jiri.strouhal <jiri.strouhal@gmail.com>
2023-11-08 13:53:51 +0100

    Enable to obtain list of cars from database

--- fleetv2_http_api/impl/car_controller.py
+++ fleetv2_http_api/impl/car_controller.py
@@ -1,11 +1,52 @@
-from typing import List
+from __future__ import annotations
+
+
+from sqlalchemy import create_engine, insert, select
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column
+import dataclasses
+
+
+from typing import List, ClassVar
 from ..controllers.car_controller import Car
 
 
+engine = create_engine("sqlite+pysqlite:///:memory:")
+
+
+class Base(DeclarativeBase):  
+    pass
+
+
+@dataclasses.dataclass
+class CarBase(Base):
+    __tablename__:ClassVar[str] = "car"
+    owner:Mapped[str] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(primary_key=True)
+
+    @staticmethod
+    def from_model(model:Car)->CarBase:
+        return CarBase(owner=model.company_name, name=model.car_name)
+    @staticmethod
+    def to_model(base:CarBase)->Car:
+        return Car(car_name=base.name, company_name=base.owner)
+
+
+Base.metadata.create_all(engine)
+
 
 def cars_available()->List[Car]:  # noqa: E501
-    return list()
+    with Session(engine) as session:
+        result = session.execute(select(CarBase))
+        cars:List[Car] = list()
+        for row in result:
+            carbase = row[0]
+            cars.append(CarBase.to_model(carbase))
+        return cars
 
 
 def add_car(car:Car)->None:
-    return
+    item = CarBase.from_model(car)
+    with engine.begin() as conn:
+        stmt = insert(CarBase.__table__)
+        conn.execute(stmt, [item.__dict__])
+    
--- fleetv2_http_api/impl/tests/test_car_controller.py
+++ fleetv2_http_api/impl/tests/test_car_controller.py
@@ -9,7 +9,7 @@ from fleetv2_http_api.impl.car_controller import *
 class Test_Nothing(unittest.TestCase):
 
     def test_adding_and_retrieving_single_car(self):
-        car = Car("test_company_123", car_name="test_car_456")
+        car = Car(company_name="test_company_123", car_name="test_car_456")
         add_car(car)
         self.assertListEqual(cars_available(), [car])
 
