4e6d0331810c734f2ee843e96f8fcf1d6878bda6
jiri.strouhal <jiri.strouhal@gmail.com>
2023-11-10 08:48:46 +0100

    Test car and company name are validated with respect to given pattern.

--- fleetv2_http_api/impl/tests/test_car_controller.py
+++ fleetv2_http_api/impl/tests/test_car_controller.py
@@ -17,6 +17,26 @@ class Test_Listing_Available_Cars(unittest.TestCase):
         _add_car(car)
         self.assertListEqual(cars_available(), [car])
 
+    def test_checking_company__and_car_name_validity(self):
+        # The car can be initialized with invalid car an company name.
+        # The pattern mathching is done only when setting company/car name 
+        # of an existing car!!!
+        car = Car(company_name="valid_name", car_name="brand_new_car")
+        car.company_name = "other_valid_name"
+        self.assertEqual(car.company_name, "other_valid_name")
+        with self.assertRaises(ValueError): car.company_name = "name with spaces"
+        with self.assertRaises(ValueError): car.company_name = "Name" # lowercase is required
+        with self.assertRaises(ValueError): car.company_name = ""
+        with self.assertRaises(ValueError): car.company_name = "  "
+
+        car.car_name = "relativelly_new_car"
+        self.assertEqual(car.car_name, "relativelly_new_car")
+        with self.assertRaises(ValueError): car.car_name = "name with spaces"
+        with self.assertRaises(ValueError): car.car_name = "Name" # lowercase is required
+        with self.assertRaises(ValueError): car.car_name = ""
+        with self.assertRaises(ValueError): car.car_name = "  "
+
+
 
 class Test_PostgreSQL_Database(unittest.TestCase):
 
