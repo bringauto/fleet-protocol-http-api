9c1a004c04c393739e1df6c4e0719caf104df9fe
jiri.strouhal <jiri.strouhal@gmail.com>
2023-11-13 14:30:39 +0100

    Add tests for accessing devices on a car

--- fleetv2_http_api/impl/database_controller.py
+++ fleetv2_http_api/impl/database_controller.py
@@ -45,6 +45,7 @@ def _new_connection_source(
     )->Engine:
 
     url = ('').join([dialect,'+',dbapi,"://",username,":",password,"@",dblocation])
+    print(f"Connecting to {url}.")
     return create_engine(url, *args, **kwargs)
 
 
@@ -58,9 +59,11 @@ def set_connection_source(
     **kwargs
     )->None:
 
+
     source = _new_connection_source(dialect, dbapi, dblocation, username, password, *args, **kwargs)
     global _connection_source
     _connection_source = source
+    print(f"New connection source: {_connection_source}")
     assert(_connection_source is not None)
     Base.metadata.create_all(source)
     
