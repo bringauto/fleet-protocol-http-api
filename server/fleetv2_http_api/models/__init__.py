e95c10fbf2985550345fec1878b4ea8663a48f2a
jiri.strouhal <jiri.strouhal@gmail.com>
2023-11-14 09:47:57 +0100

    Remove automatically generated files from git index

--- server/fleetv2_http_api/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-# import models into model package
-from fleetv2_http_api.models.car import Car
-from fleetv2_http_api.models.device_id import DeviceId
-from fleetv2_http_api.models.message import Message
-from fleetv2_http_api.models.module import Module
-from fleetv2_http_api.models.payload import Payload
