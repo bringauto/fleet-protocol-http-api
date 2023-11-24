784c5de4649c9456c6e532e668b8f366ff00b43f
jiri.strouhal <jiri.strouhal@gmail.com>
2023-11-24 14:41:17 +0100

    Create Python script for adding new item to api_key database

--- server/tests/test_new_admin.py
+++ server/tests/test_new_admin.py
@@ -9,21 +9,21 @@ from database.security import number_of_admins
 import os
 
 
-DB_FILE_LOCATION = "/server/tests/example.db"
+DB_FILE_LOCATION = "/tests/example.db"
 
 
 class Test_Adding_Admin(unittest.TestCase):
 
     def setUp(self) -> None: # pragma: no cover
-        if os.path.exists("server/tests/example.db"): 
-            os.remove("server/tests/example.db")
+        if os.path.exists("tests/example.db"): 
+            os.remove("tests/example.db")
         set_db_connection("sqlite","pysqlite",DB_FILE_LOCATION)
 
     def test_adding_admin(self):
         subprocess.run(
             [
                 'python3', 
-                'server/new_admin.py',
+                'new_admin.py',
                 'Alice',
                 "sqlite",
                 "pysqlite",
@@ -34,14 +34,14 @@ class Test_Adding_Admin(unittest.TestCase):
         self.assertEqual(number_of_admins(), 1)
 
     def test_repeatedly_adding_admin_with_the_name_has_no_effect(self):
-        args = ['python3', 'server/new_admin.py', 'Alice', "sqlite", "pysqlite", DB_FILE_LOCATION]
+        args = ['python3', 'new_admin.py', 'Alice', "sqlite", "pysqlite", DB_FILE_LOCATION]
         subprocess.run(args, capture_output=True)
         self.assertEqual(number_of_admins(), 1)
         subprocess.run(args, capture_output=True)
         self.assertEqual(number_of_admins(), 1)
 
     def test_printing_new_admin_key_to_console(self):
-        args = ['python3', 'server/new_admin.py', 'Bob', "sqlite", "pysqlite", DB_FILE_LOCATION]
+        args = ['python3', 'new_admin.py', 'Bob', "sqlite", "pysqlite", DB_FILE_LOCATION]
         result = subprocess.run(args, capture_output=True)
         self.assertTrue("New key" in result.stdout.decode("utf-8"))
 
