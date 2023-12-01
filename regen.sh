395e3295f69d20e10ccbba64a5157294ce46c073
jiri.strouhal <jiri.strouhal@gmail.com>
2023-12-01 17:14:47 +0100

    Remove line from regen.sh that runs the server after generation

--- regen.sh
+++ regen.sh
@@ -5,4 +5,3 @@ kill $(fuser 8080/tcp)
 pushd server
 npx @openapitools/openapi-generator-cli generate -g python-flask -i ../openapi.yaml -o . -p=packageName=fleetv2_http_api
 popd
-python3 -m server &
