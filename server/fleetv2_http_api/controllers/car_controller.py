cc5d489a81f1ae7f225d6afaea0696193f7b8eb0
jiri.strouhal <jiri.strouhal@gmail.com>
2023-12-14 14:45:09 +0100

    Keep wait parameter only for status and command endpoints

--- server/fleetv2_http_api/controllers/car_controller.py
+++ server/fleetv2_http_api/controllers/car_controller.py
@@ -7,13 +7,11 @@ from fleetv2_http_api.models.car import Car  # noqa: E501
 from fleetv2_http_api import util
 
 
-def available_cars(wait=None):  # noqa: E501
+def available_cars():  # noqa: E501
     """available_cars
 
     Return list of available cars for all companies registered in the database. # noqa: E501
 
-    :param wait: An empty parameter. If specified, the method waits for predefined period of time, until some data to be sent in response are available.
-    :type wait: str
 
     :rtype: Union[List[Car], Tuple[List[Car], int], Tuple[List[Car], int, Dict[str, str]]
     """
--- server/fleetv2_http_api/controllers/module_controller.py
+++ server/fleetv2_http_api/controllers/module_controller.py
@@ -7,7 +7,7 @@ from fleetv2_http_api.models.available_devices200_response import AvailableDevic
 from fleetv2_http_api import util
 
 
-def available_devices(company_name, car_name, module_id=None, wait=None):  # noqa: E501
+def available_devices(company_name, car_name, module_id=None):  # noqa: E501
     """available_devices
 
     Return device Ids for all devices available for contained in the specified car.&lt;br&gt; For a single car module, the device Ids are returned as an object containing module Id and the list of device Ids. &lt;br&gt; If a module Id is specified, only a single such object is returned. &lt;br&gt; Otherwise, a list of such objects is returned, one for each module contained in the car. &lt;br&gt; # noqa: E501
@@ -18,8 +18,6 @@ def available_devices(company_name, car_name, module_id=None, wait=None):  # noq
     :type car_name: str
     :param module_id: An Id of module, an unsigned integer.
     :type module_id: int
-    :param wait: An empty parameter. If specified, the method waits for predefined period of time, until some data to be sent in response are available.
-    :type wait: str
 
     :rtype: Union[AvailableDevices200Response, Tuple[AvailableDevices200Response, int], Tuple[AvailableDevices200Response, int, Dict[str, str]]
     """
