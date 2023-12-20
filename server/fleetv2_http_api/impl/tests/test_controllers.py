a8ab03861ba555edb0b6677c955cb3a6d14d99b7
jiri.strouhal <jiri.strouhal@gmail.com>
2023-12-20 10:52:48 +0100

    Re-enable skipped test case Test_Cleaning_Up_Commands

--- server/fleetv2_http_api/impl/tests/test_controllers.py
+++ server/fleetv2_http_api/impl/tests/test_controllers.py
@@ -1,21 +1,3 @@
-# Fleet Protocol v2 HTTP API 
-# Copyright (C) 2023 BringAuto s.r.o.
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-
 import os
 import sys
 sys.path.append("server")
@@ -37,6 +19,9 @@ from fleetv2_http_api.models.device_id import DeviceId
 from fleetv2_http_api.models.message import Payload, Message
 from fleetv2_http_api.models.module import Module
 from fleetv2_http_api.models.car import Car
+
+from database.database_controller import remove_old_messages
+from database.database_controller import future_command_warning, MessageBase
 
 
 from unittest.mock import patch, Mock
@@ -108,11 +93,7 @@ class Test_Listing_Available_Devices_And_Cars(unittest.TestCase):
             device_id=device_id, 
             payload=Payload(message_type=MessageType.STATUS_TYPE, encoding=EncodingType.JSON, data={"message":"Device is running"})
         )
-        send_statuses(
-            company_name="the_company", 
-            car_name="available_car", 
-            messages=[status]
-        )
+        send_statuses("the_company", "available_car", messages=[status])
         self.assertEqual(available_devices("the_company", "unavailable_car"), ([], 404))
         self.assertEqual(available_devices("no_company", "unavailable_car"), ([], 404))
 
@@ -129,16 +110,8 @@ class Test_Listing_Available_Devices_And_Cars(unittest.TestCase):
             device_id=device_2_id, 
             payload=Payload(message_type=MessageType.STATUS_TYPE, encoding=EncodingType.JSON, data={"message":"Device is running"})
         )
-        send_statuses(
-            company_name="the_company", 
-            car_name="available_car", 
-            messages=[status_1]
-        )
-        send_statuses(
-            company_name="the_company", 
-            car_name="available_car", 
-            messages=[status_2]
-        )
+        send_statuses("the_company", "available_car", messages=[status_1])
+        send_statuses("the_company", "available_car", messages=[status_2])
         self.assertEqual(
             available_devices("the_company", "available_car", module_id=18)[0], 
             Module(module_id=18, device_list=[device_1_id])
@@ -382,99 +355,93 @@ class Test_Options_For_Listing_Multiple_Commands(unittest.TestCase):
         self.assertEqual(commands[2].timestamp, 45)
 
 
-
-from database.database_controller import remove_old_messages
-from database.database_controller import future_command_warning, MessageBase
-@unittest.skip("this test case will be enabled after implementiung setting of the timestamp update")
 class Test_Cleaning_Up_Commands(unittest.TestCase):
 
-    COMPANY_1_NAME = "company_1"
-    CAR_A_NAME = "car_A"
-    CAR_B_NAME = "car_B"
     DATA_RETENTION_PERIOD = MessageBase.data_retention_period_ms
-
 
     def setUp(self) -> None:
         set_test_db_connection("/:memory:")
         clear_device_ids()
-
         self.device_id = DeviceId(module_id=42, type=5, role="left_light", name="Left light")
         self.status_payload = Payload(message_type=MessageType.STATUS_TYPE, encoding=EncodingType.JSON, data={"message":"Device is conected"})
         self.command_payload = Payload(message_type=MessageType.COMMAND_TYPE, encoding=EncodingType.JSON, data={"message":"Beep"})
-        
-        sdevice_id = serialized_device_id(self.device_id)
-        self.args = self.COMPANY_1_NAME, self.CAR_A_NAME, sdevice_id
-
+    
 
     @patch('database.time._time_in_ms')
-    def test_cleaning_up_commands(self, mock_timestamp):
+    def test_cleaning_up_commands(self, mock_time_in_ms:Mock):
         status = Message(timestamp=0, device_id=self.device_id, payload=self.status_payload)
         command_1 = Message(timestamp=10, device_id=self.device_id, payload=self.command_payload)
         command_2 = Message(timestamp=20, device_id=self.device_id, payload=self.command_payload)
 
-        send_statuses(*self.args, [status])
-        send_commands(*self.args, [command_1])
-        send_commands(*self.args, [command_2])
+        mock_time_in_ms.return_value = 0
+        send_statuses("company", "car", [status])
+        mock_time_in_ms.return_value = 10
+        send_commands("company", "car", [command_1])
+        mock_time_in_ms.return_value = 20
+        send_commands("company", "car", [command_2])
 
-        self.assertEqual(len(list_statuses(*self.args, all_available="")[0]), 1)
-        self.assertEqual(len(list_commands(*self.args, all_available="")[0]), 2)
+        self.assertEqual(len(list_statuses("company", "car", all_available="")[0]), 1)
+        self.assertEqual(len(list_commands("company", "car", all_available="")[0]), 2)
 
-        mock_timestamp.return_value = self.DATA_RETENTION_PERIOD+20
-        remove_old_messages(mock_timestamp.return_value)
+        mock_time_in_ms.return_value = self.DATA_RETENTION_PERIOD+20
+        remove_old_messages(mock_time_in_ms.return_value)
 
-        self.assertEqual(len(list_statuses(*self.args, all_available="")[0]), 0)
-        self.assertEqual(len(list_commands(*self.args, all_available="")[0]), 0)
+        self.assertEqual(len(list_statuses("company", "car", all_available="")[0]), 0)
+        self.assertEqual(len(list_commands("company", "car", all_available="")[0]), 0)
         
-        mock_timestamp.return_value += 5 
+        mock_time_in_ms.return_value += 5 
         
         # the following status is considered to be the FIRST status for given device and after sending it, 
         # all commands previously sent to this device have to be removed
-        send_statuses(*self.args, [status])
-        self.assertEqual(len(list_statuses(*self.args, all_available="")[0]), 1)
-        self.assertEqual(len(list_commands(*self.args, all_available="")[0]), 0)
+        send_statuses("company", "car", [status])
+        self.assertEqual(len(list_statuses("company", "car", all_available="")[0]), 1)
+        self.assertEqual(len(list_commands("company", "car", all_available="")[0]), 0)
 
 
     @patch('database.time._time_in_ms')
-    def test_cleaning_up_command_with_newer_timestamp_relative_to_the_first_status_raises_warning(self, mock_timestamp):
+    def test_cleaning_up_command_with_newer_timestamp_relative_to_the_first_status_raises_warning(self, mock_time_in_ms:Mock):
         status = Message(timestamp=0, device_id=self.device_id, payload=self.status_payload)
         new_first_status = Message(timestamp=self.DATA_RETENTION_PERIOD + 50, device_id=self.device_id, payload=self.status_payload)
         command_1 = Message(timestamp=self.DATA_RETENTION_PERIOD+30, device_id=self.device_id, payload=self.command_payload)
         # the following timestamp is invalid as it is set to the future relative to the next (FIRST) status
         command_2 = Message(timestamp=self.DATA_RETENTION_PERIOD+100, device_id=self.device_id, payload=self.command_payload)
 
-        send_statuses(*self.args, [status])
-
+        mock_time_in_ms.return_value = 0
+        send_statuses("company", "car", [status])
         self.assertEqual(
-            available_devices(self.COMPANY_1_NAME, self.CAR_A_NAME)[0], 
+            available_devices("company", "car")[0],
             [Module(module_id=42, device_list=[self.device_id])]
         )
-        self.assertEqual(available_cars(),[Car(self.COMPANY_1_NAME, self.CAR_A_NAME)])
+        self.assertEqual(available_cars(),[Car("company", "car")])
 
-        send_commands(*self.args, [command_1])
-        send_commands(*self.args, [command_2])
+        mock_time_in_ms.return_value = self.DATA_RETENTION_PERIOD+30
+        send_commands("company", "car", [command_1])
+        mock_time_in_ms.return_value = self.DATA_RETENTION_PERIOD+100
+        send_commands("company", "car", [command_2])
 
         remove_old_messages(self.DATA_RETENTION_PERIOD + 10)
 
-        self.assertEqual(len(list_statuses(*self.args, all_available="")[0]), 0)
+        self.assertEqual(len(list_statuses("company", "car", all_available="")[0]), 0)
         # The device is considered to be disconnected and all commands sent to it are then 
         # considered to be removed.
-        self.assertEqual(len(list_commands(*self.args, all_available="")[0]), 0)
+        self.assertEqual(len(list_commands("company", "car", all_available="")[0]), 0)
 
         self.assertEqual(
-            available_devices(self.COMPANY_1_NAME, self.CAR_A_NAME), 
+            available_devices("company", "car"), 
             ([], 404)
         )
         self.assertEqual(available_cars(), [])
 
-        warnings, code = send_statuses(*self.args, [new_first_status])
-        self.assertEqual(len(list_statuses(*self.args, all_available="")[0]), 1)
-        self.assertEqual(len(list_commands(*self.args, all_available="")[0]), 0) 
+        mock_time_in_ms.return_value = self.DATA_RETENTION_PERIOD + 50
+        warnings, code = send_statuses("company", "car", [new_first_status])
+        self.assertEqual(len(list_statuses("company", "car", all_available="")[0]), 1)
+        self.assertEqual(len(list_commands("company", "car", all_available="")[0]), 0) 
 
         assert(type(warnings) is str)
         for warning in future_command_warning(
                     timestamp=self.DATA_RETENTION_PERIOD + 100,
-                    company_name=self.COMPANY_1_NAME,
-                    car_name=self.CAR_A_NAME,
+                    company_name="company",
+                    car_name="car",
                     serialized_device_id= serialized_device_id(self.device_id),
                     payload_data=command_2.payload.data
                 ):
