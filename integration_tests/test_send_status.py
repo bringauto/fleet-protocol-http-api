75f63602cc86e150b2cb56e3cec17b5d7f16dc84
jiri.strouhal <jiri.strouhal@gmail.com>
2024-02-16 09:50:56 +0100

    Test sending and retrieving single status using test_client

--- integration_tests/test_send_status.py
+++ integration_tests/test_send_status.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch, Mock
 import sys
 
 sys.path.append("server")
@@ -18,6 +19,7 @@ class Test_Making_Car_Available_By_Sending_First_Status(unittest.TestCase):
             data={"phone_number": "1234567890"},
         )
         self.status = Message(device_id=self.device_id, payload=self.payload)
+        self.test_car = {"company_name":"test_company", "car_name": "test_car"}
 
     def _test_test_car_is_initially_not_among_available_cars(self) -> None:
         with self.app.app.test_client() as client:
@@ -29,7 +31,27 @@ class Test_Making_Car_Available_By_Sending_First_Status(unittest.TestCase):
             response = client.post("/v2/protocol/status/test_company/test_car", json=[self.status])
             self.assertEqual(response.status_code, 200)
             response = client.get("/v2/protocol/cars")
-            self.assertEqual(response.json, [{"company_name":"test_company", "car_name":"test_car"}])
+            self.assertEqual(response.json, [self.test_car])
+
+    @patch("fleetv2_http_api.impl.controllers.timestamp")
+    def test_retrieving_sent_status(self, mock_timestamp: Mock) -> None:
+        mock_timestamp.return_value = 11111
+        with self.app.app.test_client() as client:
+            client.post("/v2/protocol/status/test_company/test_car", json=[self.status])
+            response = client.get("/v2/protocol/status/test_company/test_car")
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                response.json,
+                [
+                    {
+                        "timestamp": 11111,
+                        "device_id": {"module_id": 7, "type": 8, "role": "test_device", "name": "Test Device"},
+                        "payload": {"message_type": "STATUS", "encoding": "JSON", "data": {"phone_number": "1234567890"}}
+                    }
+                ]
+            )
+
+
 
 
 if __name__ == "__main__":  # pragma: no cover
