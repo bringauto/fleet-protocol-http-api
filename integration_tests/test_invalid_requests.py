9bde40a8727c722ad1ec502191b745bf9a435d6b
jiri.strouhal <jiri.strouhal@gmail.com>
2024-02-22 08:12:16 +0100

    Test company and car name not matching pattern from spec yield 400 code

--- integration_tests/test_invalid_requests.py
+++ integration_tests/test_invalid_requests.py
@@ -8,7 +8,7 @@ from server.fleetv2_http_api.models.message import Message, Payload, DeviceId
 from server.enums import MessageType
 
 
-class Test_Sending_Status_With_Invalid_Request(unittest.TestCase):
+class Test_Company_And_Car_Name_Not_Following_Pattern_From_OpenAPI_Spec_Yield_Code_400(unittest.TestCase):
     def setUp(self) -> None:
         self.app = _app.get_test_app(base_url="/v2/protocol/")
         self.device_id = DeviceId(module_id=7, type=8, role="test_device", name="Test Device")
@@ -18,18 +18,51 @@ class Test_Sending_Status_With_Invalid_Request(unittest.TestCase):
             data={"phone_number": "1234567890"},
         )
         self.status = Message(device_id=self.device_id, payload=self.payload)
+        self.invalid_car_names = ["test car", "test-car", "TestCar", "    ", "$$$"]
+        self.invalid_company_names = ["test company", "test-company", "TestCompany", "    ", "$$$"]
 
-    def test_using_url_with_test_company_name_not_matching_given_pattern(self) -> None:
+    def test_status_endpoint(self) -> None:
         with self.app.app.test_client() as client:
-            response = client.post("/status/TestCompany/test_car", json=[self.status])
-            self.assertEqual(response.status_code, 400)
+            for invalid_car_name in self.invalid_car_names:
+                with self.subTest(invalid_car_name=invalid_car_name):
+                    response = client.get(f"/status/valid_company_name/{invalid_car_name}", json=[self.status])
+                    self.assertEqual(response.status_code, 400)
+                    self.assertIn("pattern", response.json["detail"]) # type: ignore
+            for invalid_company_name in self.invalid_company_names:
+                with self.subTest(invalid_company_name=invalid_company_name):
+                    response = client.get(f"/status/{invalid_company_name}/valid_car_name", json=[self.status])
+                    self.assertEqual(response.status_code, 400)
+                    self.assertIn("pattern", response.json["detail"]) # type: ignore
+
+    def test_command_endpoint(self) -> None:
         with self.app.app.test_client() as client:
-            response = client.post("/status/test_company/TestCar", json=[self.status])
-            self.assertEqual(response.status_code, 400)
+            for invalid_car_name in self.invalid_car_names:
+                with self.subTest(invalid_car_name=invalid_car_name):
+                    response = client.get(f"/command/valid_company_name/{invalid_car_name}", json=[self.status])
+                    self.assertEqual(response.status_code, 400)
+                    self.assertIn("pattern", response.json["detail"]) # type: ignore
+            for invalid_company_name in self.invalid_company_names:
+                with self.subTest(invalid_company_name=invalid_company_name):
+                    response = client.get(f"/command/{invalid_company_name}/valid_car_name", json=[self.status])
+                    self.assertEqual(response.status_code, 400)
+                    self.assertIn("pattern", response.json["detail"]) # type: ignore
+
+    def test_available_device_endpoint(self) -> None:
+        with self.app.app.test_client() as client:
+            for invalid_car_name in self.invalid_car_names:
+                with self.subTest(invalid_car_name=invalid_car_name):
+                    response = client.get(f"/available-devices/valid_company_name/{invalid_car_name}", json=[self.status])
+                    self.assertEqual(response.status_code, 400)
+                    self.assertIn("pattern", response.json["detail"]) # type: ignore
+            for invalid_company_name in self.invalid_company_names:
+                with self.subTest(invalid_company_name=invalid_company_name):
+                    response = client.get(f"/available-devices/{invalid_company_name}/valid_car_name", json=[self.status])
+                    self.assertEqual(response.status_code, 400)
+                    self.assertIn("pattern", response.json["detail"]) # type: ignore
 
     def tearDown(self) -> None:
         self.app.clear_all()
 
 
 if __name__=="__main__":
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=2, buffer=True)
