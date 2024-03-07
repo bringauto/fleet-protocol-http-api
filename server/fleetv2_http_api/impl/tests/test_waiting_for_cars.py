c4553b4f1a2b2658aadd3e3cd903e78ebda681f3
jiri.strouhal <jiri.strouhal@gmail.com>
2024-03-07 10:04:45 +0100

    Add test for timeout of request for available cars

--- server/fleetv2_http_api/impl/tests/test_waiting_for_cars.py
+++ server/fleetv2_http_api/impl/tests/test_waiting_for_cars.py
@@ -7,7 +7,9 @@ sys.path.append("server")
 from fleetv2_http_api.models import Payload, Message, Car, DeviceId  # type: ignore
 from enums import MessageType, EncodingType  # type: ignore
 from database.database_controller import set_test_db_connection, clear_device_ids  # type: ignore
-from fleetv2_http_api.impl.controllers import available_cars, send_statuses  # type: ignore
+from fleetv2_http_api.impl.controllers import (  # type: ignore
+    available_cars, send_statuses, set_car_wait_timeout_s
+)
 from _misc import run_in_threads  # type: ignore
 import time
 
@@ -35,9 +37,15 @@ class Test_Waiting_For_Available_Cars(unittest.TestCase):
             time.sleep(0.1)
             send_statuses("test_company", "test_car", [self.status_1])
         def list_cars():
-            cars = available_cars(wait=True)
-            self.assertEqual(cars[0], [Car(company_name="test_company", car_name="test_car")])
+            cars = available_cars(wait=True)[0]
+            self.assertEqual(cars, [Car(company_name="test_company", car_name="test_car")])
         run_in_threads(send_single_status, list_cars)
+
+    def test_exceeding_timeout_waiting_for_available_cars_yields_an_empty_list(self):
+        set_car_wait_timeout_s(0.1)
+        cars, code = available_cars(wait=True)
+        self.assertEqual(cars, [])
+        self.assertEqual(code, 200)
 
     def tearDown(self) -> None:
         if os.path.exists("./example.db"):
