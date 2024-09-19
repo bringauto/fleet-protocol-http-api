156370465b488265e46c345b53f1a07ac3eda716
jiri.strouhal <jiri.strouhal@gmail.com>
2024-09-19 08:30:45 +0200

    use constants to specify default logging config and output paths

--- server/logs.py
+++ server/logs.py
@@ -12,12 +12,23 @@ T = TypeVar("T", bound=Mapping)
 LOG_FILE_NAME = "fleet_protocol_http_api.log"
 LOGGER_NAME = "werkzeug"
 LOGGING_CONFIG_PATH = "config/logging.json"
+DEFAULT_LOG_DIR = "log"
 
 
 def clear_logs() -> None:
     """Clear the log file."""
-    with open(f"log/{LOG_FILE_NAME}", "w") as f:
+    log_file_path = _get_log_dir_path()
+    with open(log_file_path, "w") as f:
         f.write("")
+
+
+def _get_log_dir_path() -> str:
+    try:
+        with open(LOGGING_CONFIG_PATH) as f:
+            config = json.load(f)
+            return config["handlers"]["file"]["filename"]
+    except:
+        return DEFAULT_LOG_DIR
 
 
 def configure_logging(config_path: str) -> None:
@@ -29,10 +40,11 @@ def configure_logging(config_path: str) -> None:
         logger.warning(
             f"Fleet Protocol HTTP API: Could not find a logging configuration file (entered path: {config_path}. Using default logging configuration."
         )
-        if not os.path.isfile(f"log/{LOG_FILE_NAME}"):
-            if not os.path.exists("log"):
-                os.makedirs("log")
-            with open(f"log/{LOG_FILE_NAME}", "w") as f:
+        default_log_path = os.path.join(DEFAULT_LOG_DIR, LOG_FILE_NAME)
+        if not os.path.isfile(default_log_path):
+            if not os.path.exists(DEFAULT_LOG_DIR):
+                os.makedirs(DEFAULT_LOG_DIR)
+            with open(default_log_path, "w") as f:
                 f.write("")
 
         logger.propagate = False
@@ -40,7 +52,7 @@ def configure_logging(config_path: str) -> None:
             fmt="[%(asctime)s.%(msecs)03d] [fleet-protocol-http-api] [%(levelname)s]\t %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
-        file_handler = logging.FileHandler(f"log/{LOG_FILE_NAME}")
+        file_handler = logging.FileHandler(filename=default_log_path)
         file_handler.setLevel(level=logging.INFO)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
@@ -51,5 +63,3 @@ def configure_logging(config_path: str) -> None:
         logger.addHandler(stream_handler)
 
         logger.setLevel(level=logging.INFO)
-        if not os.path.exists("log"):
-            os.makedirs("log")
