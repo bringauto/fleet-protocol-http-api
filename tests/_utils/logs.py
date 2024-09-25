1550537af37dbf360c437b38ebd6689bbc6cb69f
jiri.strouhal <jiri.strouhal@gmail.com>
2024-09-25 10:40:01 +0200

    Remove logging.json

--- tests/_utils/logs.py
+++ tests/_utils/logs.py
@@ -1,24 +1,12 @@
 import json
-import logging
-
-from server.logs import LOGGER_NAME, DEFAULT_LOG_DIR, LOGGING_CONFIG_PATH
+import os
 
 
 def clear_logs() -> None:
     """Find and clear the log file."""
-    log_file_path = _get_log_dir_path()
+    dir = os.path.dirname(__file__)
+    config = json.loads(open(os.path.join(dir,"test_config.json")).read())
+    log_file_path = config["general-settings"]["log-path"]
     with open(log_file_path, "w") as f:
         f.write("")
 
-
-def _get_log_dir_path() -> str:
-    try:
-        with open(LOGGING_CONFIG_PATH) as f:
-            log_config = json.load(f)
-            return log_config["handlers"]["file"]["filename"]
-    except Exception as e:
-        logger = logging.getLogger(LOGGER_NAME)
-        logger.warning(
-            f"Error when attepting to find a configuration file. Using default logging configuration. The error was: {e}"
-        )
-        return DEFAULT_LOG_DIR
