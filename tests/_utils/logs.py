24537f26ed016756e027239e4d3aead8e82877fd
jiri.strouhal <jiri.strouhal@gmail.com>
2024-09-25 13:13:09 +0200

    Remove accidentally kept conflict symbols in py file

--- tests/_utils/logs.py
+++ tests/_utils/logs.py
@@ -9,21 +9,3 @@ def clear_logs() -> None:
     log_file_path = config["general-settings"]["log-path"]
     with open(log_file_path, "w") as f:
         f.write("")
-
-<<<<<<< HEAD
-
-def _get_log_dir_path() -> str:
-    try:
-        with open(LOGGING_CONFIG_PATH) as f:
-            log_config = json.load(f)
-            return log_config["handlers"]["file"]["filename"]
-    except Exception as e:
-        logger = logging.getLogger(LOGGER_NAME)
-        logger.warning(
-            f"Error when attepting to find a configuration file. Using default logging configuration. The error was: {e}"
-        )
-        return DEFAULT_LOG_DIR
-
-
-=======
->>>>>>> @{-1}
