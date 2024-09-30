b0065640f103b636f73ad36ce66d49c5a01e6e91
jiri.strouhal <jiri.strouhal@gmail.com>
2024-09-30 11:01:11 +0200

    Fix accidental cretion of empty log file in root dir

--- tests/_utils/logs.py
+++ tests/_utils/logs.py
@@ -4,9 +4,13 @@ import os
 
 def clear_logs() -> None:
     """Find and clear the log file."""
-    dir = os.path.dirname(__file__)
-    with open(os.path.join(dir, "test_config.json"), "r") as f:
+    directory = os.path.dirname(__file__)
+    with open(os.path.join(directory, "test_config.json"), "r") as f:
         config = json.load(f)
     log_file_path = config["logging"]["log-path"]
-    with open(log_file_path, "w") as f:
-        f.write("")
+    if not os.path.exists(log_file_path):
+        return
+    for file in os.listdir(log_file_path):
+        if file.endswith(".log"):
+            log_file_path = os.path.join(directory, file)
+            os.remove(log_file_path)
